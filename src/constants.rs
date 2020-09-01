42125c61599563568c445651e22622f8ac027794
Maxwell DeMers <maxuuell@gmail.com>
2020-08-31 20:46:47 -0700

    Closes #1, closes #3, closes #4

---
+++ src/constants.rs
@@ -0,0 +1,36 @@
+pub const RESOURCES: &[&str] = &[
+    "films",
+    "people",
+    "planets",
+    "species",
+    "starships",
+    "vehicles",
+];
+
+pub const RESOURCES_DATA_FILTER: &[&str] = &[
+    "all: get all of the resources",
+    "id: look up resource by id",
+    "schema: view the JSON schema for the resource",
+    "search: look up resource by name, title, etc..",
+];
+
+pub const SWAPI_BASE_PATH: &str = "http://swapi.dev/api/";
+
+pub const TICK_STRINGS: &[&str] = &[
+    "🌑 T ",
+    "🌒 Th ",
+    "🌓 Tha ",
+    "🌔 That ",
+    "🌕 That's ",
+    "🌖 That's n ",
+    "🌗 That's no ",
+    "🌘 That's no m ",
+    "🌑 That's no mo ",
+    "🌒 That's no moon ",
+    "🌓 That's no moon. ",
+    "🌔 That's no moon.. ",
+    "🌕 That's no moon... ",
+    "🌖 That's no moon.... ",
+    "🌗 That's no moon..... ",
+    "🌘 That's no moon...... ",
+];
--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,102 @@
-use exitcode;
+mod constants;
+use colored::*;
+use dialoguer::{theme::ColorfulTheme, Input, Select};
 use http::StatusCode;
 use indicatif::{ProgressBar, ProgressStyle};
-use reqwest;
 use serde_json::Value;
 use std::error::Error;
 use std::process;
-use structopt::StructOpt;
 use url::{ParseError, Url};
 
-#[derive(StructOpt, Debug)]
-struct Cli {
-    attributes: String,
-    id: String,
-}
+fn main() -> Result<(), Box<dyn Error>> {
+    let theme = &ColorfulTheme::default();
+    let mut resource_prompt = Select::with_theme(theme);
+    for resource in constants::RESOURCES {
+        resource_prompt.item(resource);
+    }
 
-fn main() -> Result<(), Box<Error>> {
-    let args = Cli::from_args();
+    println!("Choose the resource you'd like to lookup");
+    let resource_response: usize = resource_prompt.interact()?;
+    let resource = constants::RESOURCES[resource_response];
+    println!("{}", resource.green());
 
-    let paths = [&args.attributes, "/", &args.id].concat();
-    let url = build_swapi_url(&paths)?;
+    // construct the filter prompt
+    let mut resource_data_filter_prompt = Select::with_theme(theme);
 
+    for data_filter in constants::RESOURCES_DATA_FILTER {
+        resource_data_filter_prompt.item(data_filter);
+    }
+    println!("Choose the filter you'd like to use.");
+    let data_filter_response: usize = resource_data_filter_prompt.interact()?;
+    println!(
+        "{}",
+        constants::RESOURCES_DATA_FILTER[data_filter_response].green()
+    );
+
+    let path_and_arguments = if data_filter_response == 0 {
+        [resource, "/"].concat()
+    } else if data_filter_response == 1 {
+        let id = Input::<String>::new()
+            .with_prompt("What id would you like to look up?")
+            .interact()?;
+        [resource, "/", &id].concat()
+    } else if data_filter_response == 2 {
+        [resource, "/schema"].concat()
+    } else if data_filter_response == 3 {
+        // if they want to search, ask for specific input
+        if resource_response == 0 {
+            // title for films
+            let title = Input::<String>::new()
+                .with_prompt("What title would you like to lookup?")
+                .interact()?;
+
+            [resource, "/?search=", &title].concat()
+        } else if resource_response == 1 || resource_response == 2 || resource_response == 3 {
+            // name for species and planets
+            let name = Input::<String>::new()
+                .with_prompt("What name would you like to lookup?")
+                .interact()?;
+
+            [resource, "/?search=", &name].concat()
+        } else if resource_response == 4 || resource_response == 5 {
+            // name and model for starships and vehicles
+            let name_or_model = Input::<String>::new()
+                .with_prompt("What name or model would you like to lookup?")
+                .interact()?;
+
+            [resource, "/?search=", &name_or_model].concat()
+        } else {
+            String::from("")
+        }
+    } else {
+        String::from("")
+    };
+
+    // wookie prompt was working, but the response is inconsistent
+    // with different requests. Seems unstable
+    // let mut wookiee_prompt = Select::with_theme(theme);
+    // wookiee_prompt.item("No thanks");
+    // wookiee_prompt.item("huurh... means yes 😉");
+
+    // println!("You want your response, in wookiee?");
+    // let wookiee_prompt_response: usize = wookiee_prompt.interact()?;
+
+    // if wookiee_prompt_response == 1 {
+    //     path_and_arguments = [path_and_arguments, String::from("?format=wookiee")].concat();
+    // }
+
+    // https://github.com/rust-lang/rust/issues/46871#issuecomment-618186642
+
+    let url = build_swapi_url(&path_and_arguments)?;
     let pb = ProgressBar::new_spinner();
     pb.enable_steady_tick(120);
     pb.set_style(
         ProgressStyle::default_spinner()
             // For more spinners check out the cli-spinners project:
             // https://github.com/sindresorhus/cli-spinners/blob/master/spinners.json
-            .tick_strings(&[
-                "🌑 T ",
-                "🌒 Th ",
-                "🌓 Tha ",
-                "🌔 That ",
-                "🌕 That's ",
-                "🌖 That's n ",
-                "🌗 That's no ",
-                "🌘 That's no m ",
-                "🌑 That's no mo ",
-                "🌒 That's no moon ",
-                "🌓 That's no moon. ",
-                "🌔 That's no moon.. ",
-                "🌕 That's no moon... ",
-                "🌖 That's no moon.... ",
-                "🌗 That's no moon..... ",
-                "🌘 That's no moon...... ",
-            ])
+            .tick_strings(constants::TICK_STRINGS)
             .template("{spinner:.green} {msg}"),
     );
-    // https://docs.rs/reqwest/0.10.7/reqwest/blocking/struct.Response.html
     let response = reqwest::blocking::get(url)?;
     match response.status() {
         StatusCode::NOT_FOUND => {
@@ -55,13 +105,10 @@ fn main() -> Result<(), Box<Error>> {
         }
         StatusCode::OK => {
             pb.finish_with_message("Found!");
-            // https://docs.rs/reqwest/0.10.7/reqwest/blocking/struct.Response.html#optional-1
-            // Error: reqwest::Error { kind: Decode, source: Error("invalid type: map, expected unit", line: 1, column: 0) }
-            // This requires the optional json feature enabled
             let body: Value = response.json()?;
             // https://www.reddit.com/r/rust/comments/3ceaui/psa_produces_prettyprinted_debug_output/
             // https://users.rust-lang.org/t/parsing-json-response-to-just-json/47092/2?u=maxuuell
-            println!("{:#}", body)
+            println!("{:#}", body);
         }
         _ => {
             pb.finish_with_message("Something went wrong");
@@ -73,9 +120,7 @@ fn main() -> Result<(), Box<Error>> {
 }
 
 fn build_swapi_url(path: &str) -> Result<Url, ParseError> {
-    const SWAPIBASEPATH: &'static str = "http://swapi.dev/api/";
-    let url = Url::parse(SWAPIBASEPATH)?;
+    let url = Url::parse(constants::SWAPI_BASE_PATH)?;
     let url = url.join(path)?;
-
     Ok(url)
 }
